c24ccb34a6350a3d32ea34b864d4bd19d1a618d2
Your Name <you@example.com>
2020-11-27 15:15:41 +0800

    build func test

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -2,35 +2,36 @@ package org.devops
 
 
 // 构建方法 构建类型
-def Build(BuildType,BuildShell){
+def Build(BuildType, BuildShell){
+
     def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
-    BuildHome = tool buildTools[BuildType]
+//    BuildHome = tool buildTools[BuildType]
     println("当前构建的类型为: ${BuildType}")
-
-    if ("${BuildType}" == "npm"){
-
-    }else if ("${BuildType}" == "maven"){
-        println("maven")
-        sh """
-        export NODE_HOME=${BuildHome}
-        export PATH=\$NODE_HOME/bin:/$PATH
-        ${BuildHome}/bin/${BuildType} ${BuildShell}"""
-    }else if ("${BuildType}" == "gradle"){
-        println("gradle")
-        sh """
-        export NODE_HOME=${BuildHome}
-        export PATH=\$NODE_HOME/bin:/$PATH
-        ${BuildHome}/bin/${BuildType} ${BuildShell}"""
-    }else if ("${BuildType}" == "ant"){
-        println("ant")
-
-        sh """
-        export NODE_HOME=${BuildHome}
-        export PATH=\$NODE_HOME/bin:/$PATH
-        ${BuildHome}/bin/${BuildType} ${BuildShell}"""
-    }else {
-        sh "${BuildHome}/bin/${BuildType} ${BuildShell}"
-    }
+    println("非常好，已经调用到共享库方法了")
+//    if ("${BuildType}" == "npm"){
+//
+//    }else if ("${BuildType}" == "maven"){
+//        println("maven")
+//        sh """
+//        export NODE_HOME=${BuildHome}
+//        export PATH=\$NODE_HOME/bin:/$PATH
+//        ${BuildHome}/bin/${BuildType} ${BuildShell}"""
+//    }else if ("${BuildType}" == "gradle"){
+//        println("gradle")
+//        sh """
+//        export NODE_HOME=${BuildHome}
+//        export PATH=\$NODE_HOME/bin:/$PATH
+//        ${BuildHome}/bin/${BuildType} ${BuildShell}"""
+//    }else if ("${BuildType}" == "ant"){
+//        println("ant")
+//
+//        sh """
+//        export NODE_HOME=${BuildHome}
+//        export PATH=\$NODE_HOME/bin:/$PATH
+//        ${BuildHome}/bin/${BuildType} ${BuildShell}"""
+//    }else {
+//        sh "${BuildHome}/bin/${BuildType} ${BuildShell}"
+//    }
 
     // 适用于参数化构建
 
